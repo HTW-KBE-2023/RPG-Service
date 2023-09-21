e268ed76fc0848c49c91454d94361de71cd544a9
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2023-09-21 18:46:44 +0200

    Add fighting Simulation

---
+++ API/Services/Fights/FightSimulation.cs
@@ -0,0 +1,100 @@
+﻿using API.Models.Fights;
+using API.Models;
+using API.Services.Dice;
+using RabbitMQ.Client.Events;
+
+namespace API.Services.Fights
+{
+    public class FightSimulation
+    {
+        public event EventHandler<Fight>? OnComplete;
+
+        private readonly IDiceService _diceService;
+        private readonly Fight _fight;
+
+        public FightSimulation(IDiceService diceService, Fight fight)
+        {
+            _diceService = diceService;
+            _fight = fight;
+        }
+
+        public void Simulate()
+        {
+            WriteSummary($"{_fight.Player.Name} is encountering {_fight.Enemy.Name}");
+            IFightable[] _initiative = DecideInitiative(_diceService);
+            WriteSummary($"{_initiative.First().Name} has the first action!");
+
+            ExecuteFightingLoop(_diceService, _initiative);
+            WriteVictoryLog();
+
+            _fight.Completed = true;
+
+            OnComplete?.Invoke(this, _fight);
+        }
+
+        private void ExecuteFightingLoop(IDiceService dice, IFightable[] _initiative)
+        {
+            do
+            {
+                var activeFighter = _initiative[0];
+                var passiveFighter = _initiative[1];
+
+                if (dice.RollAgainst(10))
+                {
+                    WriteSummary($"{activeFighter.Name}'s attack is succesfull!");
+
+                    var attackValue = activeFighter.Attack - passiveFighter.Defence;
+                    var normalizedDamage = attackValue < 1 ? 0 : attackValue;
+
+                    passiveFighter.TakesDamage(normalizedDamage);
+
+                    WriteDamageLog(passiveFighter, normalizedDamage);
+                }
+                else
+                {
+                    WriteSummary($"{activeFighter.Name}'s attack missed!");
+                }
+
+                Array.Reverse(_initiative);
+            } while (_fight.Enemy.Health > 1 && _fight.Player.Health > 1);
+        }
+
+        private void WriteVictoryLog()
+        {
+            if (_fight.Player.Health > 0)
+            {
+                WriteSummary($"{_fight.Player.Name} won the fight and gets {_fight.Enemy.Experience} Experience as a reward!");
+            }
+            else
+            {
+                WriteSummary($"{_fight.Player.Name} lost the fight against {_fight.Enemy.Name}!");
+            }
+        }
+
+        private void WriteDamageLog(IFightable passiveFighter, int damage)
+        {
+            if (passiveFighter.Health > 0)
+            {
+                WriteSummary($"{passiveFighter.Name}'s health takes a hit of {damage} damage, now with only {passiveFighter.Health} health left!");
+            }
+            else
+            {
+                WriteSummary($"{passiveFighter.Name} has no health left and was defeated!");
+            }
+        }
+
+        private void WriteSummary(string text) => _fight.Summary.Add(text);
+
+        private IFightable[] DecideInitiative(IDiceService dice)
+        {
+            return new List<(int initative, IFightable character)>
+        {
+                { (dice.Roll() + _fight.Player.Level, _fight.Player) },
+                { (dice.Roll() + _fight.Enemy.Level , _fight.Enemy) }
+            }
+            .OrderBy(selection => selection.initative)
+            .Select(selection => selection.character)
+            .ToArray();
+        }
+    }
+}
---
+++ API/Services/Fights/FightSimulator.cs
@@ -0,0 +1,91 @@
+﻿using API.Models.Fights;
+using API.Models.Monsters;
+using API.Models.Players;
+using API.Services.Dice;
+using Boxed.Mapping;
+using MassTransit;
+using MessagingContracts.RPG;
+
+namespace API.Services.Fights
+{
+    public class FightSimulator
+    {
+        private readonly IDiceService _diceService;
+        private readonly IGenericService<Fight> _fightService;
+        private readonly IGenericService<Player> _playerService;
+        private readonly IGenericService<Monster> _monsterService;
+        private readonly IMapper<Fight, FightConcluded> _fightToFightConcludedMapper;
+        private readonly IBus _bus;
+
+        public FightSimulator(IDiceService diceService,
+                              IGenericService<Fight> fightService,
+                              IGenericService<Player> playerService,
+                              IGenericService<Monster> monsterService,
+                              FightMapper fightmapper,
+                              IBus bus)
+        {
+            _diceService = diceService;
+            _fightService = fightService;
+            _playerService = playerService;
+            _monsterService = monsterService;
+            _fightToFightConcludedMapper = fightmapper;
+            _bus = bus;
+        }
+
+        public void SimulationFor(FightRequested fightRequest)
+        {
+            Fight fight = PrepareForSimulation(fightRequest.Player);
+
+            _fightService.Create(fight);
+
+            var simulation = new FightSimulation(_diceService, fight);
+            simulation.OnComplete += NotifyServices;
+        }
+
+        private void NotifyServices(object? sender, Fight fight)
+        {
+            _fightService.Update(fight);
+
+            var concludedMessage = _fightToFightConcludedMapper.Map(fight);
+            _bus.Publish(concludedMessage);
+        }
+
+        private Fight PrepareForSimulation(Guid playerId)
+        {
+            return new()
+            {
+                Enemy = GetRandomMonster(),
+                Player = GetOrCreatePlayer(playerId)
+            };
+        }
+
+        private Monster GetRandomMonster()
+        {
+            var monsters = _monsterService.GetAll();
+            var monster = monsters.ElementAt(Random.Shared.Next(0, monsters.Count()));
+
+            return monster;
+        }
+
+        private Player GetOrCreatePlayer(Guid playerId)
+        {
+            var player = _playerService.GetById(playerId);
+
+            if (player is null)
+            {
+                var result = _playerService.Create(new Player()
+                {
+                    Name = "Default Name",
+                    Level = 1,
+                    Experience = 0,
+                    Attack = 5,
+                    Defence = 5,
+                    Health = 15
+                });
+
+                player = result.Value ?? throw new ArgumentException($"Player for Id {playerId} could not be found or createt inside of the FightSimulator.", nameof(playerId));
+            }
+            return player;
+        }
+    }
+}
