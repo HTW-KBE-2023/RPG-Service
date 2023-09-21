3c2d34118cf05ddd18d2788a36f4e29996a2e2e0
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2023-09-21 18:47:56 +0200

    Configure Application for remaining Services

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,14 @@
 using API.Utility;
+using FluentValidation;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddDatabaseConfiguration();
+builder.AddRabbitMqConfiguration();
+
+builder.AddApplicationServices();
+builder.AddMapper();
+builder.Services.AddValidatorsFromAssemblyContaining<Program>(ServiceLifetime.Singleton);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
--- API/Utility/WebApplicationBuilderExtensions.cs
+++ API/Utility/WebApplicationBuilderExtensions.cs
@@ -1,4 +1,12 @@
-﻿using API.Port.Database;
+﻿using API.Models.Fights;
+using API.Models.Monsters;
+using API.Models.Players;
+using API.Models.Validations;
+using API.Port.Database;
+using API.Port.Repositories;
+using API.Services;
+using API.Services.Dice;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Utility
@@ -13,6 +21,54 @@ namespace API.Utility
                 options.UseMySql(connection, ServerVersion.AutoDetect(connection));
             });
             builder.Services.AddTransient<DbInitialiser>();
+            builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static void AddApplicationServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IDiceService, DiceService>(_ => new DiceService(Random.Shared, DiceValue.D20));
+            builder.Services.AddScoped<IGenericService<Fight>, GenericService<Fight>>();
+            builder.Services.AddScoped<IGenericService<Player>, GenericService<Player>>();
+            builder.Services.AddScoped<IGenericService<Monster>, GenericService<Monster>>();
+        }
+
+        public static void AddMapper(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddSingleton<FightMapper, FightMapper>();
+            builder.Services.AddSingleton<PlayerMapper, PlayerMapper>();
+            builder.Services.AddSingleton<MonsterMapper, MonsterMapper>();
+            builder.Services.AddSingleton<ValidationFailedMapper, ValidationFailedMapper>();
+        }
+
+        public static void AddRabbitMqConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMassTransit(x =>
+            {
+                x.SetKebabCaseEndpointNameFormatter();
+                x.SetInMemorySagaRepositoryProvider();
+
+                var assembly = typeof(Program).Assembly;
+
+                x.AddConsumers(assembly);
+                x.AddSagaStateMachines(assembly);
+                x.AddSagas(assembly);
+                x.AddActivities(assembly);
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    var connection = builder.Configuration.GetConnectionString("RabbitMQ");
+                    cfg.Host(connection, "/", h =>
+                    {
+                        var rabbitMQConfiguration = builder.Configuration.GetSection("RabbitMQ.Configuration");
+                        var user = rabbitMQConfiguration.GetValue<string>("User");
+                        var password = rabbitMQConfiguration.GetValue<string>("Password");
+
+                        h.Username(user);
+                        h.Password(password);
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
         }
     }
 }
