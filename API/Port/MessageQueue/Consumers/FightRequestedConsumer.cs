d8d58cd8a7ede0f30743fa130099c75b8930523f
KaronisInProgram <19214686+KaronisInProgram@users.noreply.github.com>
2023-09-21 18:47:12 +0200

    Add Consumer for Fightrequests

---
+++ API/Port/MessageQueue/Consumers/FightRequestedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using API.Models.Fights;
+using API.Models.Monsters;
+using API.Models.Players;
+using API.Services;
+using API.Services.Dice;
+using API.Services.Fights;
+using MassTransit;
+using MessagingContracts.RPG;
+
+namespace API.Port.MessageQueue.Consumers;
+
+public class FightRequestedConsumer : IConsumer<FightRequested>
+{
+    private readonly ILogger<FightRequestedConsumer> _logger;
+    private readonly FightSimulator _fightSimulator;
+
+    public FightRequestedConsumer(ILogger<FightRequestedConsumer> logger, FightSimulator fightSimulator)
+    {
+        _logger = logger;
+        _fightSimulator = fightSimulator;
+    }
+
+    public Task Consume(ConsumeContext<FightRequested> context)
+    {
+        var requestedFight = context.Message;
+        _logger.LogInformation("Request a fight for Player {player}", requestedFight.Player);
+
+        _fightSimulator.SimulationFor(requestedFight);
+
+        return Task.CompletedTask;
+    }
+}
